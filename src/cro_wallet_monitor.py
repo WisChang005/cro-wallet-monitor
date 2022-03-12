319a16cdc6ffcc14305127b108e0af29ddd7966c
Wis Chang <wis_chang@trendmicro.com>
2022-03-12 13:56:14 +0800

    feat: add CRO prise parser

--- src/cro_wallet_monitor.py
+++ src/cro_wallet_monitor.py
@@ -14,7 +14,8 @@ def main():
 
 def get_sending_message() -> str:
     cro_wallet_counts = get_cro_wallet_counts()
-    message = f"CRO Wallet addresses: {cro_wallet_counts}"
+    cro_prise = get_cro_price()
+    message = f"\nCRO Prise ($USD): {cro_prise}\nCRO Wallet addresses: {cro_wallet_counts}"
     return message
 
 
@@ -39,6 +40,19 @@ def line_notify(line_token: str, msg: str):
     line_status = r.status_code
     logging.debug("Line notify status code [%s]", line_status)
     return line_status
+
+
+def get_cro_price() -> str:
+    url = "https://coinmarketcap.com/currencies/cronos/holders/"
+    headers = {
+        "User-Agent": ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+                       "(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.62"),
+        "Accept-Language": "en-US,en;q=0.9"
+    }
+    rsp = requests.get(url, headers=headers)
+    bs4 = BeautifulSoup(rsp.text, "lxml")
+    cro_prise = bs4.find("div", {"class": "priceValue"}).span.text
+    return cro_prise
 
 
 if __name__ == "__main__":
